20267410913cf69ce36bc8d26b07298a0c395f26
HAIDALA <91802818+HAIDALA@users.noreply.github.com>
2022-10-05 11:39:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { configure } from "https://deno.land/x/eta@v1.12.3/mod.ts";
 import * as taskController from "./controllers/taskController.js";
+import * as workEntryController from "./controllers/workEntryController.js";
+import * as requestUtils from "./utils/requestUtils.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
 });
 
 const handleRequest = async (request) => {
-  const url = new URL(request.url);
-
-  if (url.pathname === "/" && request.method === "GET") {
-    return new Response(`Redirecting to /tasks.`, {
-      status: 303,
-      headers: {
-        "Location": "/tasks",
-      },
-    });
-  } else if (url.pathname === "/tasks" && request.method === "POST") {
-    return await taskController.addTask(request);
-  } else if (url.pathname === "/tasks" && request.method === "GET") {
-    return await taskController.viewTasks(request);
-  } else {
-    return new Response("Not found", { status: 404 });
-  }
-};
-
+    const url = new URL(request.url);
+  
+    if (url.pathname === "/" && request.method === "GET") {
+      return requestUtils.redirectTo("/tasks");
+    } else if (url.pathname === "/tasks" && request.method === "POST") {
+      return await taskController.addTask(request);
+    } else if (url.pathname === "/tasks" && request.method === "GET") {
+      return await taskController.viewTasks(request);
+    } else if (url.pathname.match("tasks/[0-9]+") && request.method === "GET") {
+      return await taskController.viewTask(request);
+    } else if (url.pathname.match("tasks/[0-9]+/entries/[0-9]+") && request.method === "POST") {
+      return await workEntryController.finishWorkEntry(request);
+    } else if (url.pathname.match("tasks/[0-9]+/entries") && request.method === "POST") {
+      return await workEntryController.createWorkEntry(request);
+    } else if (url.pathname.match("tasks/[0-9]+") && request.method === "POST") {
+      return await taskController.completeTask(request);
+    } else {
+      return new Response("Not found", { status: 404 });
+    }
+  };
 serve(handleRequest, { port: 7777 });
