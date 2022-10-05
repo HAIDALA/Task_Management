a0dbabbf7cd041ee0049d4fd6e45dd4e74aad195
HAIDALA <91802818+HAIDALA@users.noreply.github.com>
2022-10-05 11:43:01 +0200

    Update taskService.js

--- services/taskService.js
+++ services/taskService.js
@@ -1,5 +1,11 @@
 import { executeQuery } from "../database/database.js";
 
+const completeById = async (id) => {
+  await executeQuery("UPDATE tasks SET completed = true WHERE id = $id;", {
+    id: id,
+  });
+};
+
 const create = async (name) => {
   await executeQuery("INSERT INTO tasks (name) VALUES ($name);", {
     name: name,
@@ -10,7 +16,20 @@ const findAllNonCompletedTasks = async () => {
   let result = await executeQuery(
     "SELECT * FROM tasks WHERE completed = false;"
   );
+
   return result.rows;
 };
 
-export { create, findAllNonCompletedTasks };
+const findById = async (id) => {
+  let result = await executeQuery("SELECT * FROM tasks WHERE id = $id;", {
+    id: id,
+  });
+
+  if (result.rows && result.rows.length > 0) {
+    return result.rows[0];
+  }
+
+  return { id: 0, name: "Unknown" };
+};
+
+export { completeById, create, findAllNonCompletedTasks, findById };
